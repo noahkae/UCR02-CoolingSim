fe2994ddde5ed9437d224eaa2756a6c6b175da11
Noah Elsayed <noah.elsayed@ucalgary.ca>
2025-03-02 22:15:14 -0700

    radiator testing begins

--- model_support_script.m
+++ model_support_script.m
@@ -76,3 +76,31 @@ function y_out = fit_and_interpolate(x)
     x_interp = linspace(1, length(x_clean), 50);
     y_out = polyval(p, x_interp);
 end
+
+%--------------------------------------------------------------------------
+% Aliexpress rad dimensions and properties
+radiator_L = 0.24;
+radiator_W = 0.012;
+radiator_H = 0.12;
+tubes_N = 18;
+tube_H = 0.00135;
+fin_spacing = 0.001;
+wall_thickness = 0.0001;
+wall_conductivity = 237;
+liquid_pipe_D = 0.01;
+
+tube_H_internal = tube_H - 2 * wall_thickness; % [m]
+tube_W_internal = radiator_W - 2 * wall_thickness; % [m]
+
+gap_H = (radiator_H - tubes_N * tube_H) / (tubes_N - 1); % [m]
+air_area_flow = (tubes_N - 1) * radiator_L * gap_H; % [m^2]
+
+air_area_primary = tubes_N * 2 * (radiator_W + tube_H) * radiator_L; % [m^2]
+
+fins_N = (tubes_N - 1) * radiator_L / fin_spacing;
+air_area_fins = 2 * fins_N * radiator_W * gap_H; % [m^2]
+
+thermal_resistance_primary = wall_thickness / air_area_primary / wall_conductivity; % [K/kW]
+thermal_resistance_secondary = 1/(55/1000);
+
+liquid_pipe_A = pi * liquid_pipe_D^2 / 4; % [m^2]
